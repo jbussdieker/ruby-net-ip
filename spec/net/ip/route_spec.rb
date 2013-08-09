777ada63c4ee705dc9667d1eb286ab04bd776f20
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-09 10:38:43 -0400

    Fixup rake tasks

--- spec/net/ip/route_spec.rb
+++ spec/net/ip/route_spec.rb
@@ -4,9 +4,4 @@ describe Net::IP::Route do
   it "should be creatable" do
     Net::IP::Route.new.should be_kind_of(Net::IP::Route)
   end
-
-  it "should build param string" do
-    route = Net::IP::Route.new(:via => "1.1.1.1", :dev => "eth0", :weight => 1)
-    route.build_param_string.should eql("via 1.1.1.1 dev eth0 weight 1")
-  end
 end
